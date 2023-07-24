da59c577d5b45e0cc9447485e9f63a80cc7ea89c
Ariful islam <arifulislam10111999@gmail.com>
2023-07-24 15:06:12 +0600

    calculate total

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -134,30 +134,30 @@ const cartReducer = (state, action) => {
   //   };
   // }
 
-  // if (action.type === "CART_ITEM_PRICE_TOTAL") {
-  //   let { total_item, total_price } = state.cart.reduce(
-  //     (accum, curElem) => {
-  //       let { price, amount } = curElem;
+  if (action.type === "CART_ITEM_PRICE_TOTAL") {
+    let { total_item, total_price } = state.cart.reduce(
+      (accum, curElem) => {
+        let { price, amount } = curElem;
 
         
 
-  //       accum.total_item += amount;
-  //       accum.total_price += price * amount;
+        accum.total_item += amount;
+        accum.total_price += price * amount;
 
-  //       return accum;
-  //     },
-  //     {
-  //       total_item: 0,
-  //       total_price: 0,
-  //     }
-  //   );
-  //   console.log(state)
-  //   return {
-  //     ...state,
-  //     total_item,
-  //     total_price,
-  //   };
-  // }
+        return accum;
+      },
+      {
+        total_item: 0,
+        total_price: 0,
+      }
+    );
+    console.log(state)
+    return {
+      ...state,
+      total_item,
+      total_price,
+    };
+  }
 
   return state;
 };
